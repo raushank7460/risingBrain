70c75a202772f9a5f1fa72ef0e6b3a68e464033b
Raushan Kumar <raushank7460@gmail.com>
2026-09-14 18:09:26 +0530

    update

---
+++ TwoPointer/validPalindrome.java
@@ -0,0 +1,37 @@
+package TwoPointer;
+
+public class validPalindrome {
+    public  static  boolean isPalindrome(String s){
+        int n=s.length();
+        int l=0;
+        int r=n-1;
+
+        while(l<r){
+            if(!Character.isLetterOrDigit(s.charAt(l))){
+                l++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(s.charAt(r))){
+                r--;
+                continue;
+            }
+            if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r))){
+                return false;
+
+            }
+            l++;
+            r--;
+        }
+        return  true;
+    }
+    public static void main(String[] args) {
+        String s="race a car";
+        //  String s="apa";
+        System.out.println(isPalindrome(s));
+
+        
+        
+        
+    }
+    
+}
