93f1cc32e8209c2a7ef630fade6ffcdb794e0c44
Raushan Kumar <raushank7460@gmail.com>
2026-09-17 17:39:17 +0530

    add

---
+++ tcs/OddEven.java
@@ -0,0 +1,15 @@
+package tcs;
+
+public class OddEven {
+    
+    public static void main(String[] args) {
+        int num=56;
+       if(num%2==0){
+        System.out.println("Even");
+       }else{
+        System.out.println("Odd");
+       }
+        
+    }
+    
+}
---
+++ tcs/posNigZero.java
@@ -0,0 +1,16 @@
+package tcs;
+// to check the given number is  positive or negitive or Zero
+public class posNigZero {
+    public static void main(String[] args) {
+        int num=0;
+        if(num>0){
+            System.out.println("+Ve");
+        }else if(num<0){
+            System.out.println("-Ve");
+
+        }else{
+            System.out.println("Zeros");
+        }
+    }
+    
+}
