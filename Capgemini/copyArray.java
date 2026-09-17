230cbaeb976c3806ce39f4e52711bc8ba9258b36
Raushan Kumar <raushank7460@gmail.com>
2026-09-18 00:28:59 +0530

    update

---
+++ Capgemini/copyArray.java
@@ -0,0 +1,12 @@
+package Capgemini;
+
+
+import java.util.*;
+
+public class copyArray {
+    public static void main(String[] args) {
+        int[]nums={1,2,3,4,5};
+        int[]copy=Arrays.copyOf(nums, nums.length);
+    }
+    
+}
---
+++ Capgemini/sortAsd.java
@@ -0,0 +1,12 @@
+package Capgemini;
+
+import java.util.Arrays;
+
+public class sortAsd {
+    public static void main(String[] args) {
+        int[]nums={9,6,4,2,5,1,3};
+        Arrays.sort(nums);
+        System.out.println(nums);
+    }
+    
+}
