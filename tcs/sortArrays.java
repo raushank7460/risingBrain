9968350df822050365a626ed6093fd0b12aeeec5
Raushan Kumar <raushank7460@gmail.com>
2026-09-19 22:34:34 +0530

    update

---
+++ tcs/sortArrays.java
@@ -0,0 +1,14 @@
+package tcs;
+
+import java.util.Arrays;
+
+public class sortArrays {
+    public static void main(String[] args) {
+        int[]nums={9,8,7,6,5};
+        Arrays.sort(nums);
+        for(int ele:nums){
+            System.out.println(ele+"");
+        }
+    }
+    
+}
