da3ddd6031ef7f286d0f66dc56a1db05b663b370
Raushan Kumar <raushank7460@gmail.com>
2026-09-22 00:19:21 +0530

    update

--- tcs/validPalindrome.java
+++ tcs/validPalindrome.java
@@ -18,7 +18,7 @@ public class validPalindrome {
     }
     
     public static void main(String[] args) {
-        String str="abc";
+        String str="A man, a plan, a canal: Panama";
         System.out.println(isPalindrome(str));
     }
         
