33c992cfa308c435493e6f3b907f345a09bc626c
Raushan Kumar <raushank7460@gmail.com>
2026-09-24 23:14:42 +0530

    update

--- DP/climbstairs.java
+++ DP/climbstairs.java
@@ -30,20 +30,23 @@ public class climbstairs {
     
 // }
 
+public int climbStairs(int n) {
 
-public static int solve(int n){
-    
-    if(n<=1){
-        return 1;
+        if (n <= 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
-    int []dp=new int[n+1];
-    dp[0]=0;
-    dp[1]=1;
-    for(int i=2;i<=n;i++){
-        dp[i]=solve(n-1)+solve(n-2);
-    }
-    return dp[n];
-}
         
     
     
