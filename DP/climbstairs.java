5e0632dea72213d45bb8fde0022cd17ffc3cea5a
Raushan Kumar <raushank7460@gmail.com>
2026-09-24 23:07:46 +0530

    climbstairs

---
+++ DP/climbstairs.java
@@ -0,0 +1,50 @@
+package DP;
+
+
+
+public class climbstairs {
+    public static void main(String[] args) {
+        int n=3;
+         System.out.println(solve(n));
+
+    }
+    // using recursion method
+    // public static int solve(int n){
+    //     if(n<=1){
+    //         return 1;
+    //     }
+    //     return solve(n-1)+solve(n-2);
+// }
+
+// public static int solve(int n){
+//     int[]dp=new int[46];
+//     if(n<=1){
+//         return 1;
+//     }
+
+//     if(dp[n]!=0){
+//         return dp[n];
+//     }
+//     dp[n]=solve(n-1)+solve(n-2);
+//     return dp[n];
+    
+// }
+
+
+public static int solve(int n){
+    
+    if(n<=1){
+        return 1;
+    }
+    int []dp=new int[n+1];
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=solve(n-1)+solve(n-2);
+    }
+    return dp[n];
+}
+        
+    
+    
+}
